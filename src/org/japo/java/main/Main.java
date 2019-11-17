6313672952dae92d74ed63b18810eadf7f0b3fe0
Giusy <Giusy@192.168.1.129>
2019-11-17 19:14:03 +0100

    :tada: Primer Commit

---
+++ src/org/japo/java/main/Main.java
@@ -0,0 +1,38 @@
+/* 
+ * Copyright 2019 Josefina Pugliese Vazquez - josefinapwork@gmail.com.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.japo.java.main;
+
+/**
+ *
+ * @author Josefina Pugliese Vazquez
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        
+        //Constantes
+        final int NUM_MAX = 10;
+        
+        //Variable
+        int i = 0;
+        
+        while (i < NUM_MAX) {
+            System.out.println(i);
+            i++;            
+        }
+        
+    }
+}
